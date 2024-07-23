15ac099479f90306457cff38df168af37c835547
Abdel <abdelbarka96@gmail.com>
2024-07-23 14:45:33 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Outfit: ['Outfit', 'sans-serif'],
+        YoungSerif: ['YoungSerif', 'serif'],
+      },
+      colors: {
+        'rcp-white': 'hsl(0, 0%, 100%)',
+        'rcp-stone': {
+          100: 'hsl(30, 54%, 90%)',
+          150: 'hsl(30, 18%, 87%)',
+          600: 'hsl(30, 10%, 34%)',
+          900: 'hsl(24, 5%, 18%)',
+        },
+        'rcp-brown':{
+          800: 'hsl(14, 45%, 36%)',
+        },
+        'rcp-rose': {
+          800: 'hsl(332, 51%, 32%)',
+          50: 'hsl(330, 100%, 98%)',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
